ebc0fe390ac4a2bbffb77f0771e448463208c665
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-08 22:20:10 -0300

    Create example file for array and object destructuring in ES6

---
+++ 04-rest-spread.js
@@ -0,0 +1,11 @@
+// Arrays desctructuring
+let fruit = ["Apple", "Banana"];
+
+let [a, b] = fruit;
+console.log(a, b);
+
+// Object destructructuring
+let user = { username: "Pepe", age: 25 };
+
+let { username, age } = user;
+console.log(username, user.age);
