6d489c78e10a278ae7887918961652242ce6527e
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-10 22:24:34 -0300

    Add example of fuction with Spread Operator in ES6

--- 05-spread-operator.js
+++ 05-spread-operator.js
@@ -10,6 +10,45 @@ let data = { id: 1, ...person, country };
 
 console.log(data); // Output: { id: 1, name: "Pepe", age: 28, country: "MX" }
 
+/*
+Ayuda a entender y resolver este ejercicio de "Combina objetos JSON con el Spread Operator"
+En este desafío recibirás dos objetos JSON por parámetros.
+
+Usando el spread operator, deberás combinar ambos objetos en uno solo. Es posible que no se te envíe alguno de los dos objetos o ninguno, por lo que deberás usar estos por defecto, en su respectivo orden:
+
+JSON 1:
+``` JavaScript
+
+{
+    name: "Mr. Michi",
+    food: "Pescado"
+}```
+
+JSON 2:
+``` JavaScript
+
+{
+    age: 12,
+    color: "Blanco"
+}```
+
+La solución deberá tener un input y output como la siguiente, recuerda que se te pueden o no enviar los parámetros.
+*/
+
+// Definición de la función 'solution'
+function solution(
+  json1 = { name: "Mr. Michi", food: "Pescado" },
+  json2 = { age: 12, color: "Blanco" }
+) {
+  // Combinar 'json1' y 'json2' usando el Spread Operator
+  const combinedJson = { ...json1, ...json2 };
+  return combinedJson;
+}
+
+// Ejemplo de uso:
+const result = solution({ name: "Bigotes", food: "Pollito" });
+console.log(result); // Output: { name: "Bigotes", food: "Pollito", age: 12, color: "Blanco" }
+
 // Rest (Parámetros Rest)
 // Los Parámetros Rest permiten representar un número indefinido de argumentos como un array.
 // Se usan en la definición de la función, precedidos por '...' antes del nombre del parámetro que
